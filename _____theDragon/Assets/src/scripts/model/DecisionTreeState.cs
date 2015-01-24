3756ad0bef8744336fbd95d898d258d41a4647f8
Qwertyyupitsme <shasta_the_dude@rogers.com>
2015-01-24 14:00:16 -0500

    Added basic decision tree state class

---
+++ _____theDragon/Assets/src/scripts/model/DecisionTreeState.cs
@@ -0,0 +1,11 @@
+﻿public class DecisionTreeState : IDecisionTreeState {
+	private string info;
+	
+	public DecisionTreeState(string info) {
+		this.info = info;
+	}
+	
+	public string GetStateInfo() {
+		return info;
+	}
+}
