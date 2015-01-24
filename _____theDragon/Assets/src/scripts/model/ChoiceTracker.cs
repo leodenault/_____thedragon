412e24d77b77783a73a253c50b44611229fed367
Qwertyyupitsme <shasta_the_dude@rogers.com>
2015-01-24 13:49:55 -0500

    Added implementation of choice tracker which implments the IChoiceTracker interface

---
+++ _____theDragon/Assets/src/scripts/model/ChoiceTracker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class ChoiceTracker : IChoiceTracker {
+	private List<IDecisionTreeState> states;
+	
+	public ChoiceTracker() {
+		this.states = new List<IDecisionTreeState>();
+	}
+	
+	public IDecisionTreeState[] getStates() {
+		return states.ToArray();
+	}
+	
+	public void addState(IDecisionTreeState state) {
+		states.Add(state);
+	}
+}
