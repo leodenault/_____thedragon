3dcd8d96776a7c0fa4ef82effeebb379c147e474
thurston <deanthurston2791@gmail.com>
2015-01-24 11:03:20 -0500

    Added Basic Movement Controls
    Made movement controls and basic collision with edges of tiles

---
+++ _____theDragon/Assets/src/scripts/CharacterControls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControls : MonoBehaviour {
+
+	// Use this for initialization
+	Vector2 offset;
+	Vector2 max, min;
+	public Transform tile;
+	public float speed;
+	float buffer = 0.2f;
+	void Start () 
+	{
+		
+		getEdges();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Camera.main.transform.position = new Vector3(transform.position.x,transform.position.y,-10);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 moveDir = new Vector3(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical" ), 0); 
+		if((moveDir.x > 0 && transform.position.x >= max.x - buffer) || (moveDir.x < 0 && transform.position.x <= min.x + buffer)) 
+		{
+			moveDir.x = 0;
+		}
+
+		if((moveDir.y > 0 && transform.position.y >= max.y - buffer) || (moveDir.y < 0 && transform.position.y <= min.y + buffer)) 
+		{
+			moveDir.y = 0;
+		}
+
+		transform.position+= moveDir; 
+	}
+
+	public void getEdges() // finds the world position of the walls around the  tile for collision
+	{
+		offset = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize); 
+		max = new Vector2((tile.position.x + (tile.localScale.x / 2f)) - offset.x, (tile.position.y + (tile.localScale.y / 2f)) - offset.y);
+		min = new Vector2((tile.position.x - (tile.localScale.x / 2f)) + offset.x, (tile.position.y - (tile.localScale.y / 2f)) + offset.y);
+	}
+}
