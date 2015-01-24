b315eb25e3862857eae4174fbd985cc84d280a5f
Qwertyyupitsme <shasta_the_dude@rogers.com>
2015-01-24 15:04:48 -0500

    Added initial work for decision tree

---
+++ _____theDragon/Assets/src/scripts/model/DecisionTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public static class DecisionTree {
+	private static IDecisionTreeState start;
+	
+	private static IDecisionTreeState current;
+	
+	public static void Init() {
+		initStates();
+		setupChoices();
+	}
+	
+	private static void initStates() {
+		start = new DecisionTreeState("");
+		
+		current = start;
+	}
+	
+	private static void setupChoices() {
+		start.AddChoice("LOL");
+		start.AddChoice("NOPE");
+	}
+	
+	public static List<string> GetChoices() {
+		return current.GetChoices();
+	}
+}
--- _____theDragon/Assets/src/scripts/model/DecisionTreeState.cs
+++ _____theDragon/Assets/src/scripts/model/DecisionTreeState.cs
@@ -1,11 +1,23 @@
-﻿public class DecisionTreeState : IDecisionTreeState {
+﻿using System.Collections.Generic;
+
+public class DecisionTreeState : IDecisionTreeState {
 	private string info;
+	private List<string> choices;
 	
 	public DecisionTreeState(string info) {
 		this.info = info;
+		this.choices = new List<string>();
 	}
 	
 	public string GetStateInfo() {
 		return info;
+	}
+	
+	public void AddChoice(string choice) {
+		choices.Add(choice);
+	}
+	
+	public List<string> GetChoices() {
+		return choices;
 	}
 }
