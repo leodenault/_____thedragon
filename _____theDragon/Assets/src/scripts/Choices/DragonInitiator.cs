849ecfefa3aa46d7d19f3eb0d291a86ecf54efb5
Qwertyyupitsme <shasta_the_dude@rogers.com>
2015-01-25 10:02:47 -0500

    Restructured decision trigger to allow for multiple instantiations and triggering at any time

---
+++ _____theDragon/Assets/src/scripts/Choices/DragonInitiator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonInitiator : MonoBehaviour {
+
+	public ChoicePanel choicePanel;
+
+	// Use this for initialization
+	void Start () {
+		TriggerDecision decision = new TriggerDecision(choicePanel);
+		decision.Trigger();
+	}
+}
