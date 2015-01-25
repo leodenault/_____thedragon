38c5888b70cbf8538e9267f555b821d443ff277b
bokeef01 <brian.o_keefe@tufts.edu>
2015-01-25 13:36:44 -0500

    Basic Title Screen

---
+++ _____theDragon/Assets/src/scripts/FlashingText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class FlashingText : MonoBehaviour {
+
+	int timer = 100;
+	public Text flash;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		timer--;
+		if (timer <= 0) {
+			timer = 100;
+			if (!flash.IsActive()) {
+				flash.transform.active = true;
+			} else {
+				flash.transform.active = false;
+			}
+			//timer = 100;
+		}
+	}
+}
