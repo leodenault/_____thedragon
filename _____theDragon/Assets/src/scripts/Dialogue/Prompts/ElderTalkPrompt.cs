1be8ba6a7ec540005c611538b60e272277b59d96
bokeef01 <brian.o_keefe@tufts.edu>
2015-01-25 04:46:02 -0500

    Elder talk prompt

---
+++ _____theDragon/Assets/src/scripts/Dialogue/Prompts/ElderTalkPrompt.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElderTalkPrompt : MonoBehaviour {
+
+	public DialogueDisplay disp;
+	bool once = true;
+	bool first = true;
+	CircleCollider2D col;
+
+	// Use this for initialization
+	void Start () {
+		DecisionTree.Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		if (col.transform.tag == "Player" && !col.isTrigger && (first||!once) && Input.GetKey(KeyCode.Space)) {
+			if (DecisionTree.IsCurrentState ("start") && once && first) {
+				disp.displaytext (2);
+				once = false;
+			} else if (DecisionTree.IsCurrentState ("start") && !once && first) {
+				disp.displaytext (3);
+				first = false;
+			} else if (!first && !once) {
+				disp.displaytext (4);
+				once = true;
+			}
+
+			//}
+		}
+		once = false;
+	}
+}
