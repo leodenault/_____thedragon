409c45573d27f8c99e61ff39cb3a988d99c5d4c2
bokeef01 <brian.o_keefe@tufts.edu>
2015-01-25 08:13:09 -0500

    Village Prompt improvement

--- _____theDragon/Assets/src/scripts/Dialogue/Prompts/ElderTalkPrompt.cs
+++ _____theDragon/Assets/src/scripts/Dialogue/Prompts/ElderTalkPrompt.cs
@@ -7,13 +7,12 @@ public class ElderTalkPrompt : MonoBehaviour {
 	bool once = true;
 	bool first = true;
 	bool start = false;
-	CircleCollider2D col;
+	bool trigger = false;
+	public GameObject character;
 
 	// Use this for initialization
 	void Start () {
 		DecisionTree.Init ();
-		//move the sprite to the old man
-		start = true;
 	}
 	
 	// Update is called once per frame
@@ -22,18 +21,29 @@ public class ElderTalkPrompt : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		//if (col.transform.tag == "Player" && !col.isTrigger && (first||!once) && Input.GetKey(KeyCode.Space)) {
+		if (character.transform.position [0] >= 0 && !trigger) {
+			start = true;
+			trigger = true;
+		}
+			//if (col.transform.tag == "Player" && !col.isTrigger && (first||!once) && Input.GetKey(KeyCode.Space)) {
 			if (DecisionTree.IsCurrentState ("start") && once && first && start) {
 				disp.displaytext (2);
 				once = false;
-			first = true;
+				first = true;
 			} else if (DecisionTree.IsCurrentState ("start") && !once && first && Input.GetKeyDown(KeyCode.Space)) {
 				disp.displaytext (3);
 				first = false;
+				start = false;
 			} else if (DecisionTree.IsCurrentState("start") && !first && !once && Input.GetKeyDown(KeyCode.Return)) {
 				disp.displaytext (4);
 				once = true;
 			}
 		//}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.transform.tag == "Player" && !col.isTrigger) {
+			start = true;
+		}
 	}
 }
