44bdd9b7fd43d669dc581e42686e618614a55be3
Qwertyyupitsme <shasta_the_dude@rogers.com>
2015-01-25 10:14:43 -0500

    Removed duplicate merge conflict

--- _____theDragon/Assets/src/scripts/TriggerDecision.cs
+++ _____theDragon/Assets/src/scripts/TriggerDecision.cs
@@ -12,16 +12,7 @@ public class TriggerDecision : DecisionTree.Listener {
 	public void Trigger()
 	{
 		DecisionTree.registerListener(this);
-<<<<<<< Updated upstream
-
-//		string[] options = {"kill dragon", "talk to dragon"};
-//		choicePanel.GenerateOptions(options);
-//=======
 		choicePanel.GenerateOptions(DecisionTree.GetChoices().ToArray());
-//>>>>>>> f60cff2762743ac9d9e625d48898a71a22ad88bc
-=======
-		choicePanel.GenerateOptions(DecisionTree.GetChoices().ToArray());
->>>>>>> Stashed changes
 		choicePanel.Display(true);
 	}
 	
