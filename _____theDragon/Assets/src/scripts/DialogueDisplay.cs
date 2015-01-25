091ef0ac4e2b50b6a1d2e62f15980f6a7f37330b
thurston <deanthurston2791@gmail.com>
2015-01-24 19:32:53 -0500

    Made Dialogue use new UI system

--- _____theDragon/Assets/src/scripts/DialogueDisplay.cs
+++ _____theDragon/Assets/src/scripts/DialogueDisplay.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DialogueDisplay : MonoBehaviour {
 
@@ -7,13 +8,17 @@ public class DialogueDisplay : MonoBehaviour {
 	public int displayedChars;
 	public string dialogue;
 	public int textWidth = 40;
-	public GUIText TextDisp;
-
+	public Text TextDisp;
+	int spot;
+	bool scrolling;
 	// Use this for initialization
 	void Start () {
+		spot = 0;
+		scrolling = false;
 		displayedChars = 0;
 		dialogue = "";
 		TextDisp.text = dialogue;
+		displaytext();
 		//put character into dialogue state
 		//check if the dialogue has options?
 		//determine which text to display
@@ -24,10 +29,27 @@ public class DialogueDisplay : MonoBehaviour {
 	void Update () {
 	
 	}
+	
+	void FixedUpdate()
+	{
+		if(scrolling)
+		{
+			if(spot < dialogue.Length)
+			{
+				TextDisp.text = dialogue.Substring(0,spot);
+				spot++;
+			}
+			else
+			{
+				scrolling = false;
+			}
+		}
+	}
 
 	string getDialogue(int dialogueNumber) {
 		//get the correct text from another function
-		return "This is 20 char now. Let's see what happens when we add more characters. We'll see when we see."; //placeholder
+		return "This is 20 char now. Let's see what happens when we add more characters. We'll see when we see. what happens when " +  
+				"like i give the string a ridiculous amput of text, like way more thast we wou;d eeeeeeeeeever need, but its good to test this shot ya know?"; //placeholder
 	}
 	
 	int getNumChars (int stringMarker) {
@@ -52,25 +74,28 @@ public class DialogueDisplay : MonoBehaviour {
 		return charNum;
 	}
 	
-	void displaytext () {
+	public void displaytext () {
 
 		bool done = false;
 		bool pressed = false;
 		int dialogueMarker = 0; //where you are in the string
+		scrolling = true;
 		dialogue = getDialogue (dialogueNumber);
 		//TextDisp.text = "out of getDialogue";
 		//int lineNum = 0; //how many lines of text are currently displayed
 		//int charNum = 0; //how many characters are currently displayed
-		while (!done) {
+		/*while (!done) {
 			dialogueMarker = displayFragment (dialogueMarker);
 			if (dialogueMarker >= dialogue.Length) {
 				done = true;
 				break;
-			}
+			}*/
 			/*while (!pressed) {
 				StartCoroutine (keyPress(pressed));
 			}*/
-		}
+		//}
+		TextDisp.transform.parent.active = true;
+		TextDisp.text = dialogue;
 	}
 
 	IEnumerator keyPress (bool pressed) {
