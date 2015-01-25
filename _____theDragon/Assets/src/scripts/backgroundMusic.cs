e8c738a36c794de07f7a1d45f7c80b26baf7fb3f
Aniket Zodpey <azodpey@ccs.neu.edu>
2015-01-25 13:43:03 -0500

    Audio file added

---
+++ _____theDragon/Assets/src/scripts/backgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundMusic : MonoBehaviour {
+
+	private GameObject obj;
+	public AudioSource bckgrnd;
+
+	public AudioClip s;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		bckgrnd.clip = s;
+		bckgrnd.Play();
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
